564def09af4c357c39316edc46d076f458e944ca
zjun <565061763@qq.com>
2024-09-15 20:06:51 +0800

    first commit

---
+++ app/src/main/java/com/sd/demo/compose/active/SampleActive.kt
@@ -0,0 +1,112 @@
+package com.sd.demo.compose.active
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sd.demo.compose.active.theme.AppTheme
+import com.sd.lib.compose.active.FActive
+import com.sd.lib.compose.active.FActiveLaunchedEffect
+import com.sd.lib.compose.active.FActiveLifecycle
+import com.sd.lib.compose.active.fIsActive
+
+class SampleActive : ComponentActivity() {
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContent {
+         AppTheme {
+            FActiveLifecycle {
+               Content()
+            }
+         }
+      }
+   }
+}
+
+@Composable
+private fun Content() {
+   FActiveLaunchedEffect { active ->
+      logMsg { "FActiveLaunchedEffect active:$active" }
+   }
+
+   Box(
+      modifier = Modifier
+         .fillMaxSize()
+         .padding(10.dp)
+   ) {
+      StateBox(text = "1", activeColor = Color.Green) {
+         StateBox(text = "2", activeColor = Color.Red) {
+            StateBox(text = "3", activeColor = Color.Blue) {
+               StateBox(text = "4", activeColor = Color.Yellow) {
+                  StateBox(activeColor = Color.Cyan)
+               }
+            }
+         }
+      }
+   }
+}
+
+@Composable
+private fun StateBox(
+   modifier: Modifier = Modifier,
+   text: String = "",
+   activeColor: Color,
+   child: (@Composable () -> Unit)? = null,
+) {
+   var checked by remember { mutableStateOf(false) }
+
+   Column(
+      modifier = modifier
+         .fillMaxSize()
+         .border(2.dp, if (fIsActive()) activeColor else Color.Gray)
+         .padding(10.dp),
+      horizontalAlignment = Alignment.CenterHorizontally,
+   ) {
+      Row(
+         modifier = Modifier.fillMaxWidth(),
+         verticalAlignment = Alignment.CenterVertically,
+         horizontalArrangement = Arrangement.Center,
+      ) {
+         Text(text = text)
+         if (child != null) {
+            Spacer(modifier = Modifier.width(10.dp))
+            Switch(checked = checked, onCheckedChange = { checked = it })
+         }
+      }
+
+      if (child != null) {
+         FActive(active = checked) {
+            child()
+         }
+      }
+   }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DefaultPreview() {
+   AppTheme {
+      Content()
+   }
+}
---
+++ lib/src/main/java/com/sd/lib/compose/active/Active.kt
@@ -0,0 +1,87 @@
+package com.sd.lib.compose.active
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import kotlinx.coroutines.CoroutineScope
+
+private val LocalActive = compositionLocalOf<Boolean?> { null }
+
+/**
+ * 当前位置是否处于激活状态
+ */
+@Composable
+fun fIsActive(): Boolean {
+   return checkNotNull(LocalActive.current) { "Not in FActive scope." }
+}
+
+/**
+ * 根据[minActiveState]决定[content]是否处于激活状态，当状态大于等于[minActiveState]时处于激活状态
+ */
+@Composable
+fun FActiveLifecycle(
+   minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+   content: @Composable () -> Unit,
+) {
+   val lifecycleOwner = LocalLifecycleOwner.current
+
+   var lifecycleActive by remember(lifecycleOwner, minActiveState) {
+      mutableStateOf(lifecycleOwner.lifecycle.currentState.isAtLeast(minActiveState))
+   }
+
+   DisposableEffect(lifecycleOwner, minActiveState) {
+      val observer = LifecycleEventObserver { _, event ->
+         lifecycleActive = event.targetState.isAtLeast(minActiveState)
+      }
+      lifecycleOwner.lifecycle.addObserver(observer)
+      onDispose {
+         lifecycleOwner.lifecycle.removeObserver(observer)
+      }
+   }
+
+   FActive(
+      active = lifecycleActive,
+      content = content,
+   )
+}
+
+/**
+ * 根据[active]决定[content]是否处于激活状态
+ */
+@Composable
+fun FActive(
+   active: Boolean,
+   content: @Composable () -> Unit,
+) {
+   val localActive = LocalActive.current
+   val finalActive = if (localActive == null) active else (active && localActive)
+
+   CompositionLocalProvider(LocalActive provides finalActive) {
+      content()
+   }
+}
+
+/**
+ * 当[keys]或者当前位置的激活状态发生变化时，会触发[block]
+ */
+@Composable
+fun FActiveLaunchedEffect(
+   vararg keys: Any?,
+   block: suspend CoroutineScope.(active: Boolean) -> Unit,
+) {
+   val blockUpdated by rememberUpdatedState(block)
+   val isActive = fIsActive()
+   LaunchedEffect(isActive, *keys) {
+      blockUpdated(isActive)
+   }
+}
