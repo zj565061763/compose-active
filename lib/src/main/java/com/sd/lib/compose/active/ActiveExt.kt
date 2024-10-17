c9dbad01b60a05aa4ff75ff0b94ba0aa74560a56
zjun <565061763@qq.com>
2024-10-18 01:03:49 +0800

    collectAsStateWithActive

--- lib/src/main/java/com/sd/lib/compose/active/ActiveExt.kt
+++ lib/src/main/java/com/sd/lib/compose/active/ActiveExt.kt
@@ -2,28 +2,52 @@ package com.sd.lib.compose.active
 
 import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
-import androidx.compose.runtime.produceState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.withContext
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
 @Composable
-private fun <T> StateFlow<T>.fCollectAsStateWithActive(
+fun <T> StateFlow<T>.fCollectAsStateWithActive(
+   context: CoroutineContext = EmptyCoroutineContext,
+): State<T> {
+   @SuppressLint("StateFlowValueCalledInComposition")
+   val initialValue = this.value
+   return collectAsStateWithActive(
+      initialValue = initialValue,
+      context = context,
+   )
+}
+
+@Composable
+fun <T> Flow<T>.fCollectAsStateWithActive(
+   initialValue: T,
+   context: CoroutineContext = EmptyCoroutineContext,
+): State<T> {
+   return collectAsStateWithActive(
+      initialValue = initialValue,
+      context = context,
+   )
+}
+
+@Composable
+private fun <T> Flow<T>.collectAsStateWithActive(
+   initialValue: T,
    context: CoroutineContext = EmptyCoroutineContext,
 ): State<T> {
    val flow = this
-   @SuppressLint("StateFlowValueCalledInComposition")
-   val initialValue = flow.value
-   val isActive = fIsActive()
-   return produceState(initialValue = initialValue, flow, isActive, context) {
-      if (isActive) {
-         if (context == EmptyCoroutineContext) {
-            flow.collect { this@produceState.value = it }
-         } else {
-            withContext(context) {
-               flow.collect { this@produceState.value = it }
+   return remember { mutableStateOf(initialValue) }.also { state ->
+      if (fIsActive()) {
+         LaunchedEffect(flow, context) {
+            if (context == EmptyCoroutineContext) {
+               flow.collect { state.value = it }
+            } else withContext(context) {
+               flow.collect { state.value = it }
             }
          }
       }
