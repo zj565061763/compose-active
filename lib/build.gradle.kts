7f0b3ad33deb31001126304fd17a056365823b0b
zjun <565061763@qq.com>
2024-10-23 12:16:39 +0800

    1.1.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-active"
-val libVersion = "1.1.0-beta03"
+val libVersion = "1.1.0"
 
 android {
    namespace = "com.sd.lib.compose.active"
