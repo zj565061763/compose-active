4bfad755c7417fe443e16aa4144ea5b4a30ba98f
zjun <565061763@qq.com>
2024-11-07 11:55:43 +0800

    FActiveIndex

--- lib/src/main/java/com/sd/lib/compose/active/ActivePagerExt.kt
+++ lib/src/main/java/com/sd/lib/compose/active/ActivePagerExt.kt
@@ -2,13 +2,22 @@ package com.sd.lib.compose.active
 
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun PagerState.FActiveIndex(
    index: Int,
    content: @Composable () -> Unit,
 ) {
-   val active = !isScrollInProgress && currentPage == index
+   var active by remember { mutableStateOf(false) }
+
+   if (!isScrollInProgress) {
+      active = currentPage == index
+   }
+
    FActive(
       active = active,
       content = content,
