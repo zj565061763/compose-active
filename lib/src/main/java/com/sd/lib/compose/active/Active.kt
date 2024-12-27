5490e3b1229a5dad394c01eeccd1128e6f3d2749
zjun <565061763@qq.com>
2024-12-27 14:29:33 +0800

    comment

--- lib/src/main/java/com/sd/lib/compose/active/Active.kt
+++ lib/src/main/java/com/sd/lib/compose/active/Active.kt
@@ -2,13 +2,7 @@ package com.sd.lib.compose.active
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import kotlinx.coroutines.delay
 
 private val LocalActive = compositionLocalOf<Boolean?> { null }
 
@@ -21,7 +15,7 @@ fun fIsActive(): Boolean {
 }
 
 /**
- * 根据[active]决定[content]是否处于激活状态
+ * 根据[active]设置[content]是否处于激活状态
  */
 @Composable
 fun FSetActive(
