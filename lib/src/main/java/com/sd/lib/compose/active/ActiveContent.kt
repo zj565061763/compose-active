448031c5a66a5d759699117d873dc1f7846632a0
zjun <565061763@qq.com>
2024-12-27 13:01:27 +0800

    ActiveContent

---
+++ lib/src/main/java/com/sd/lib/compose/active/ActiveContent.kt
@@ -0,0 +1,63 @@
+package com.sd.lib.compose.active
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.delay
+
+/**
+ * 激活状态才会加载[content]，当状态由激活变为未激活时，过[getActiveTimeout]毫秒后移除[content]
+ */
+@Composable
+inline fun FActiveContent(
+  getActiveTimeout: () -> Long = { 0 },
+  default: @Composable () -> Unit = {},
+  content: @Composable () -> Unit,
+) {
+  val isActive = fIsActive()
+  var hasActive by remember { mutableStateOf(isActive) }
+
+  if (isActive) {
+    hasActive = true
+  } else {
+    if (hasActive) {
+      val timeout = getActiveTimeout()
+      if (timeout > 0) {
+        LaunchedEffect(Unit) {
+          delay(timeout)
+          hasActive = false
+        }
+      } else {
+        hasActive = false
+      }
+    }
+  }
+
+  if (hasActive) {
+    content()
+  } else {
+    default()
+  }
+}
+
+/**
+ * 至少激活过一次，才会加载[content]
+ */
+@Composable
+inline fun FActiveAtLeastOnce(
+  default: @Composable () -> Unit = {},
+  content: @Composable () -> Unit,
+) {
+  val isActive = fIsActive()
+  var hasActive by remember { mutableStateOf(isActive) }
+
+  if (hasActive || isActive) {
+    hasActive = true
+    content()
+  } else {
+    default()
+  }
+}
